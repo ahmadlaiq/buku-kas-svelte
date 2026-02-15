8e181abf9b2061ea5008a97b2752f6c2898c4dc3
ahmadlaiq97 <ahmadlaiq97@gmail.com>
2026-02-15 09:13:55 +0700

    fix: replace nodePolyfills with manual aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		nodePolyfills({
-			include: ['events', 'fs', 'util', 'stream', 'path', 'crypto', 'dns', 'net', 'tls'],
-			globals: {
-				Buffer: true,
-				global: true,
-				process: true,
-			},
-		}),
-	],
+	plugins: [sveltekit()],
 	ssr: {
 		noExternal: ['@prisma/adapter-pg']
 	},
 	optimizeDeps: {
 		exclude: ['@prisma/adapter-pg', 'pg-native']
+	},
+	resolve: {
+		alias: {
+			stream: 'stream-browserify',
+			crypto: 'crypto-browserify',
+			zlib: 'browserify-zlib',
+			path: 'path-browserify',
+            events: 'events',
+		}
 	},
     define: {
         global: 'globalThis',
