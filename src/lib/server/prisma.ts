d043ad9ee8deed208ab6a6c44b1ee919f8502730
ahmadlaiq97 <ahmadlaiq97@gmail.com>
2026-02-15 08:04:49 +0700

    fix: use dynamic env for DATABASE_URL

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
-import { DATABASE_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 const Pool = pg.Pool || (pg as any).default?.Pool || (pg as any).Pool;
 
-const connectionString = `${DATABASE_URL}`;
+const connectionString = `${env.DATABASE_URL}`;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
