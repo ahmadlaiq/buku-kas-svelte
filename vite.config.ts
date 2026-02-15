bfa2594843123bb74c844c85419ddc5df6fc9faf
ahmadlaiq97 <ahmadlaiq97@gmail.com>
2026-02-15 08:23:18 +0700

    fix: polyfill node builtins for cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		nodePolyfills({
+			include: ['events', 'fs', 'util', 'stream', 'path', 'crypto', 'dns', 'net', 'tls'],
+			globals: {
+				Buffer: true,
+				global: true,
+				process: true,
+			},
+		}),
+	],
 	ssr: {
 		noExternal: ['@prisma/adapter-pg']
 	},
 	optimizeDeps: {
-		exclude: ['@prisma/adapter-pg']
-	}
+		exclude: ['@prisma/adapter-pg', 'pg-native']
+	},
+    define: {
+        global: 'globalThis',
+    },
 });
